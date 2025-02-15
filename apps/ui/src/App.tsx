9f530d1bf8f736a1c862e452a9edd6ff46bafe61
emeraldjava <paul.t.oconnell@gmail.com>
2025-02-15 11:28:03 +0000

    Link the UI with the lib - broken?

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -2,9 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {handleOperation,Operation} from '@wma-agegrade-calculator/lib'
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* https://thriveread.com/create-monorepos-with-pnpm-workspaces/ */
+  const [num1,setNum1] = useState(0);
+  const [num2,setNum2] = useState(0);
+  const [result,setResult] = useState(0);
+
+  const submitOperation = (operation:Operation) => {
+    setResult(handleOperation(num1,num2,operation));
+  }
 
   return (
     <>
@@ -18,16 +26,18 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div>
+          <p>The result is: {result}</p>
+        </div>
+        <form>
+          <input type="number" name="num1" value={num1} onChange={e => setNum1(parseFloat(e.target.value))} />
+          <input type="number" name="num2" value={num2} onChange={e => setNum2(parseFloat(e.target.value))} />
+          <button type='button' onClick={() => submitOperation(Operation.add)}>Addbutton</button>
+          <button type='button' onClick={() => submitOperation(Operation.subtract)}>Subtractbutton</button>
+          <button type='button' onClick={() => submitOperation(Operation.divide)}>Dividebutton</button>
+          <button type='button' onClick={() => submitOperation(Operation.multiply)}>Multiplybutton</button>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
