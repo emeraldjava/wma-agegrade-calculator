6ad0af0b365ce4c83b2ca0d3d2bb809107a1c109
emeraldjava <paul.t.oconnell@gmail.com>
2025-03-17 12:04:20 +0000

    Tweak the turbo config to build.

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -2,18 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { handleOperation, Operation } from "@wma-agegrade-calculator/wma-calculator-lib";
+// import { handleOperation, Operation } from "@wma-agegrade-calculator/wma-calculator-lib";
 //type handleOperation = handleOperation
 
 function App() {
   /* https://thriveread.com/create-monorepos-with-pnpm-workspaces/ */
   const [num1,setNum1] = useState(0);
   const [num2,setNum2] = useState(0);
-  const [result,setResult] = useState(0);
+  //const [result,setResult] = useState(0);
 
-  const submitOperation = (operation:Operation) => {
-    setResult(handleOperation(num1,num2,operation));
-  }
+  //const submitOperation = (operation:Operation) => {
+  //  setResult(handleOperation(num1,num2,operation));
+  //}
 
   return (
     <>
@@ -33,10 +33,10 @@ function App() {
         <form>
           <input type="number" name="num1" value={num1} onChange={e => setNum1(parseFloat(e.target.value))} />
           <input type="number" name="num2" value={num2} onChange={e => setNum2(parseFloat(e.target.value))} />
-          <button type='button' onClick={() => submitOperation(Operation.add)}>Addbutton</button>
+          {/* <button type='button' onClick={() => submitOperation(Operation.add)}>Addbutton</button>
           <button type='button' onClick={() => submitOperation(Operation.subtract)}>Subtractbutton</button>
           <button type='button' onClick={() => submitOperation(Operation.divide)}>Dividebutton</button>
-          <button type='button' onClick={() => submitOperation(Operation.multiply)}>Multiplybutton</button>
+          <button type='button' onClick={() => submitOperation(Operation.multiply)}>Multiplybutton</button> */}
         </form>
       </div>
     </>
